df82d50e6989c9cd3c24231668b3c3c7c99c89ac
Andrej Lőrinc <andrejl884@gmail.com>
2023-01-02 23:39:47 +0100

    First sync

---
+++ BackUpLinePC/UC_2_Setup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BackUpLinePC
+{
+    public partial class UC_2_Setup : UserControl
+    {
+        public UC_2_Setup()
+        {
+            InitializeComponent();
+        }
+    }
+}
